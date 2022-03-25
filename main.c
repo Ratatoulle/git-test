b44d18fc036f41ca369dd2c45e7f3de190b694ed
Ratatoulle <1vasilev.andrey@gmail.com>
2022-03-25 19:09:28 +0500

    printed string with my name

--- main.c
+++ main.c
@@ -5,10 +5,12 @@
 int main()
 {
 	int a, b, c;
+	char* text = "Andrew";
 	a = b = 5;
 	c = multiplication(a, b);
 	printf("Hello Git!\n");
 	printf("%d", a + b);
 	printf("%d", c);
+	printf(text);
 	return 0;
 }
