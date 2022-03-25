7cda8950083ed4a285868d2ea317427cf0c1e815
Ratatoulle <1vasilev.andrey@gmail.com>
2022-03-25 19:06:05 +0500

    using multiplication function in main

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "git.h"
 
 int main()
 {
-	int a, b;
+	int a, b, c;
 	a = b = 5;
+	c = multiplication(a, b);
 	printf("Hello Git!\n");
 	printf("%d", a + b);
+	printf("%d", c);
 	return 0;
 }
